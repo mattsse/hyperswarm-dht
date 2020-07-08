bed0aa6fbbf156f97b54d75fef4d2e62b907ac3c
mattsse <matthias.seitz@tum.de>
2020-07-08 22:48:17 +0200

    fix: fix deadline compare order

--- src/rpc/jobs.rs
+++ src/rpc/jobs.rs
@@ -8,7 +8,6 @@ use wasm_timer::{Delay, Instant};
 /// Periodic job.
 #[derive(Debug)]
 pub struct PeriodicJob {
-    pub next_publish: Instant,
     pub interval: Duration,
     pub inner: Delay,
 }
@@ -16,18 +15,13 @@ pub struct PeriodicJob {
 impl PeriodicJob {
     pub fn new(interval: Duration) -> Self {
         Self {
-            next_publish: Instant::now() + interval,
             interval,
             inner: Delay::new(interval),
         }
     }
 
     pub fn poll(&mut self, cx: &mut Context, now: Instant) -> Poll<()> {
-        let mut ready = self.next_publish > now;
         if let Poll::Ready(Ok(_)) = Delay::poll(Pin::new(&mut self.inner), cx) {
-            ready = true;
-        }
-        if ready {
             let deadline = now + self.interval;
             self.inner = Delay::new_at(deadline);
             Poll::Ready(())
